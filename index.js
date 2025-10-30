79760791dcb0304f5370c0bf9a682dc074330797
Shree <shreyaray5829@gmail.com>
2025-10-30 14:27:08 +0530

    node.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const loving= require("./second");
+// http server
 
-console.log("Hello world" , loving)
+const http = require("http");
+const fs = require("fs");
+
+
+const myServer = http.createServer((req , res)  => {
+    // console.log("new recording started");
+
+    // console.log(req, Headers);
+
+    const log = `${Date.now()}: New req received\n`
+    fs.appendFile("log.txt", log, (err, data) => {
+      res.end("Hello, i am here");
+});
+ 
+});
+
+  
+
+myServer.listen(1000, () => console.log("it is started"))
+
+
+// go to the localhost:1000 and refresh it ...check the terminal,  the headers will there
