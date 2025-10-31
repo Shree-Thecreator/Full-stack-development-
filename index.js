6ba43a2e782667587eb0577b9ef913e1dac43ba9
Shree <shreyaray5829@gmail.com>
2025-10-31 15:29:21 +0530

    Node.js
    Api project using node.js

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-// http server
+const express = require("express");
+const users = require("./data.json");
 
-const http = require("http");
-const fs = require("fs");
+const app = express();
+const PORT = 8000;
 
-
-const myServer = http.createServer((req , res)  => {
-    // console.log("new recording started");
-
-    // console.log(req, Headers);
-
-    const log = `${Date.now()}: New req received\n`
-    fs.appendFile("log.txt", log, (err, data) => {
-      res.end("Hello, i am here");
-});
- 
+// Route to get all users
+app.get('/users', (req, res) => {
+    res.json(users);
 });
 
-  
+// Route to get a user by ID
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id);
 
-myServer.listen(1000, () => console.log("it is started"))
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+});
+
+app.post('api/users', (req, res) => {
+    res.json({status : "pending"});
+});
+
+app.patch('api/users/:id', (req, res) => {
+    // too edit the usser with id
+    res.json({status : "pending"});
+});
+
+app.delete('api/users/:id', (req, res) => {
+    // too delete the usser with id
+    res.json({status : "pending"});
+});
 
 
-// go to the localhost:1000 and refresh it ...check the terminal,  the headers will there
+
+// Start the server
+app.listen(PORT, () => console.log(`🚀 Server started at http://localhost:${PORT}`));
